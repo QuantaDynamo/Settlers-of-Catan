408cdee7edd1c908a516a2826e2ef8e4f7aa15f3
Kayla Riggs <kmr242@cornell.edu>
2023-03-22 17:32:34 -0400

    Updated start command to display player info

--- bin/main.ml
+++ bin/main.ml
@@ -2,15 +2,12 @@ open Catan
 open Command
 open Gamestate
 open Board
+open Player
 
-type game_state = {
-  board : Board.node list;
-  player : Player.player;
-}
-let b = {
-  board = Board.node_list;
-  player = Player.player_test;
-}
+type game_state = { board : Board.node list; player : Player.player }
+
+let b = { board = Board.node_list; player = Player.player_test }
+
 let rec play_game game =
   let rec game_loop game =
     ignore (Sys.command "clear");
@@ -18,38 +15,40 @@ let rec play_game game =
     let cmd_str = read_line () in
     let cmd = parse_string cmd_str in
     match cmd with
-      | Start ->
-          ANSITerminal.print_string [ ANSITerminal.blue ]
-            "Hello! You are now playing Settlers of Caml-tan. Please enter a \
-             command. \n";
-          ANSITerminal.print_string [ ANSITerminal.blue] (Board.display b.board);
-          let new_game = play_game game in
-          game_loop new_game
-      | Quit ->
-          ANSITerminal.print_string [ ANSITerminal.blue ]
-            "Thank you for playing Settlers of Caml-tan! See you next time! \n";
-          exit 0
-      | Roll ->
-          let p, q = roll_dice () in
-          ANSITerminal.print_string [ ANSITerminal.blue ]
-            ("You've rolled a "
-            ^ string_of_int (p + q)
-            ^ " and have moved to that tile. \n");
-          ANSITerminal.print_string [ ANSITerminal.blue] (Board.display b.board);
-          game_loop game
-      | Settle ->
-        (* let nodes = Board.node_list in *)
-        let b = {board = (build_settlement 2 b.player); player = b.player} in
-        ANSITerminal.print_string [ ANSITerminal.blue] (Board.display b.board);
+    | Start ->
+        ANSITerminal.print_string [ ANSITerminal.blue ]
+          ("Hello! You are now playing Settlers of Caml-tan. The current \
+            player is "
+          ^ string_of_player player_test);
+        let nodes = Board.node_list in
+        ANSITerminal.print_string [ ANSITerminal.blue ] (Board.display nodes);
+        let new_game = play_game game in
+        game_loop new_game
+    | Quit ->
+        ANSITerminal.print_string [ ANSITerminal.blue ]
+          "Thank you for playing Settlers of Caml-tan! See you next time! \n";
+        exit 0
+    | Roll ->
+        let p, q = roll_dice () in
+        ANSITerminal.print_string [ ANSITerminal.blue ]
+          ("You've rolled a "
+          ^ string_of_int (p + q)
+          ^ " and have moved to that tile. \n");
+        ANSITerminal.print_string [ ANSITerminal.blue ] (Board.display b.board);
         game_loop game
-      | Empty ->
-          ANSITerminal.print_string [ ANSITerminal.blue ]
+    | Settle ->
+        (* let nodes = Board.node_list in *)
+        let b = { board = build_settlement 2 b.player; player = b.player } in
+        ANSITerminal.print_string [ ANSITerminal.blue ] (Board.display b.board);
+        game_loop game
+    | Empty ->
+        ANSITerminal.print_string [ ANSITerminal.blue ]
           "Please enter a command. \n";
-          game_loop game
-      | Invalid -> 
-          ANSITerminal.print_string [ ANSITerminal.blue ]
+        game_loop game
+    | Invalid ->
+        ANSITerminal.print_string [ ANSITerminal.blue ]
           "Invalid command. Please try again. \n";
-          game_loop game
+        game_loop game
   in
   game_loop game
 
