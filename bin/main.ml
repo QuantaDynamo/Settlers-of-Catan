45a7be93624f680177990e6928fc33e710f15d2c
Javier Vega <jdv67@cornell.edu>
2023-03-22 17:27:50 -0400

    Update to settle.

--- bin/main.ml
+++ bin/main.ml
@@ -3,6 +3,14 @@ open Command
 open Gamestate
 open Board
 
+type game_state = {
+  board : Board.node list;
+  player : Player.player;
+}
+let b = {
+  board = Board.node_list;
+  player = Player.player_test;
+}
 let rec play_game game =
   let rec game_loop game =
     ignore (Sys.command "clear");
@@ -14,8 +22,7 @@ let rec play_game game =
           ANSITerminal.print_string [ ANSITerminal.blue ]
             "Hello! You are now playing Settlers of Caml-tan. Please enter a \
              command. \n";
-          let nodes = Board.node_list in
-          ANSITerminal.print_string [ ANSITerminal.blue] (Board.display nodes);
+          ANSITerminal.print_string [ ANSITerminal.blue] (Board.display b.board);
           let new_game = play_game game in
           game_loop new_game
       | Quit ->
@@ -28,11 +35,13 @@ let rec play_game game =
             ("You've rolled a "
             ^ string_of_int (p + q)
             ^ " and have moved to that tile. \n");
+          ANSITerminal.print_string [ ANSITerminal.blue] (Board.display b.board);
           game_loop game
       | Settle ->
-        let nodes = Board.node_list in
-          ANSITerminal.print_string [ ANSITerminal.blue] (Board.display nodes);
-          game_loop game
+        (* let nodes = Board.node_list in *)
+        let b = {board = (build_settlement 2 b.player); player = b.player} in
+        ANSITerminal.print_string [ ANSITerminal.blue] (Board.display b.board);
+        game_loop game
       | Empty ->
           ANSITerminal.print_string [ ANSITerminal.blue ]
           "Please enter a command. \n";
