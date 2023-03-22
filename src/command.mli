702849eb40f7a51547eb2fbaa7b33852eda64838
Kayla Riggs <kmr242@cornell.edu>
2023-03-21 21:51:46 -0400

    Wrote parse_string specification in command.mli

--- src/command.mli
+++ src/command.mli
@@ -4,4 +4,5 @@ exception Invalid
 type command = Start | Quit | Roll
 
 val parse_string : string -> command
-(** [parse_string]  *)
+(** [parse_string] parses a player's string input into a command. It converts all
+    strings to lowercase to assist with usability  *)
